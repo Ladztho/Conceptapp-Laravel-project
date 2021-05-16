d19e0ae2157abf231baae73d6b20a61f67120d6a
Ladztho <kyler539@gmail.com>
2021-05-16 22:51:23 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+
+        <div class="col-3 p-5"
+
+        </div>
+        <div class="col-9">
+            <img src="/images/OS-Ubuntu-icon.png" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Conceptapp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>This is a concept</strong>  hello123</div>
+                <div class="pr-5"><strong>This is a concept 2</strong> hello456</div>
+                <div class="pr-5"><strong>This is a concept 3</strong> hello789</div>
+            </div>
+        </div>
+</div>
+@endsection
